e88c8ed54dfa9f2b5c5ab409877f2b23d7140db7
TisoczkiBenedek <benedektisoczki@gmail.com>
2022-11-08 08:43:30 +0100

    Benedek hőse

--- addHero.py
+++ addHero.py
@@ -4,5 +4,6 @@ from tkinter import *
 def addHero(root):
     #s változó ->példányosítok, létrehozok egy szuperhőst Thor névvel és 70 erőponntal
     s = Szuperhos("Thor", 70)
+    benedeké = Szuperhos("KKK", 30)
     createhHeroLbl = Label(root, text=s.nev + " " + str(s.szuperero))
     createhHeroLbl.pack()
