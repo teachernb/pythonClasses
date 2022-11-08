fd0730934edc4453b7a64d66d11980f0ab7163ff
sDataCodex <mobpeti@gmail.com>
2022-11-08 08:44:16 +0100

    Zalanhose

--- addHero.py
+++ addHero.py
@@ -4,5 +4,6 @@ from tkinter import *
 def addHero(root):
     #s változó ->példányosítok, létrehozok egy szuperhőst Thor névvel és 70 erőponntal
     s = Szuperhos("Thor", 70)
+    zalanhose = Szuperhos("Goku", 9001)
     createhHeroLbl = Label(root, text=s.nev + " " + str(s.szuperero))
     createhHeroLbl.pack()
