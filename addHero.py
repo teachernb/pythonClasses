d76c696356fc27f6854e181ddcaaffe16197182c
Somalol <tivsoma@gmail.com>
2022-11-08 08:39:33 +0100

    Soma hőse

--- addHero.py
+++ addHero.py
@@ -4,5 +4,6 @@ from tkinter import *
 def addHero(root):
     #s változó ->példányosítok, létrehozok egy szuperhőst Thor névvel és 70 erőponntal
     s = Szuperhos("Thor", 70)
+    somahosealegjobb = Szuperhos("UwU", 9999999999999999)
     createhHeroLbl = Label(root, text=s.nev + " " + str(s.szuperero))
     createhHeroLbl.pack()
