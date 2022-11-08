6a88f0cf5e297e68026d6d4f490a3d1ea703c2b0
TisoczkiBenedek <benedektisoczki@gmail.com>
2022-11-08 09:16:16 +0100

    fogonosz

--- addHero.py
+++ addHero.py
@@ -4,19 +4,6 @@ from tkinter import *
 def addHero(root):
     #s változó ->példányosítok, létrehozok egy szuperhőst Thor névvel és 70 erőponntal
     s = Szuperhos("Thor", 70)
-    benedeke = Szuperhos("KKK", 40)
-    somahosealegjobb = Szuperhos("UwU", 9999999999999999)
-    david_hose = Szuperhos("😎", -1)
-    dominikHose = Szuperhos("RHN", 1235)
-    david_hose = Szuperhos("😎", -1)
-    s1 = Szuperhos("NB", "Infinity")
-    veghdavidbelapatrik = Szuperhos("Kazuma", 9001)
-    eszterhose = Szuperhos("Hősneve", 45)
-    k = Szuperhos("Supermodell", 1000)
-
-
-
-
-    g = Szuperhos("PepsiMan", 5999999999)
+    benedeke = Szuperhos("KKK", 40000)
     createhHeroLbl = Label(root, text=s.nev + " " + str(s.szuperero))
     createhHeroLbl.pack()
