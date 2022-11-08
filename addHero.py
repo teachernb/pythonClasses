1a7c85473e8e18074ffee1cf140d9782becdb5c9
Somalol <tivsoma@gmail.com>
2022-11-08 09:15:16 +0100

    hewo

--- addHero.py
+++ addHero.py
@@ -3,12 +3,6 @@ from tkinter import *
 
 def addHero(root):
     #s változó ->példányosítok, létrehozok egy szuperhőst Thor névvel és 70 erőponntal
-    s = Szuperhos("Thor", 70)
-    dominikHose = Szuperhos("RHN", 1235)
-    david_hose = Szuperhos("😎", -1)
     somahosealegjobb = Szuperhos("UwU", 9999999999999999)
-    veghdavidbelapatrik = Szuperhos("Kazuma", 9001)
-    eszterhose = Szuperhos("Hősneve", 45)
-    k = Szuperhos("Supermodell", 1000)
-    createhHeroLbl = Label(root, text=s.nev + " " + str(s.szuperero))
+    createhHeroLbl = Label(root, text=somahosealegjobb.nev + " " + str(somahosealegjobb.szuperero))
     createhHeroLbl.pack()
