fd2886e40a1d3bf8fa18123732d6d2f7e99d42a8
Farever <nagylajosdominik@gmail.com>
2022-11-08 08:47:54 +0100

    Dominik hőse

--- addHero.py
+++ addHero.py
@@ -4,5 +4,6 @@ from tkinter import *
 def addHero(root):
     #s változó ->példányosítok, létrehozok egy szuperhőst Thor névvel és 70 erőponntal
     s = Szuperhos("Thor", 70)
+    dominikHose = Szuperhos("RHN", 1234)
     createhHeroLbl = Label(root, text=s.nev + " " + str(s.szuperero))
     createhHeroLbl.pack()
